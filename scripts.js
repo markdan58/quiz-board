83fa71e87c27db0ca988cb754118d3fbe66950d7
markdan58 <mark.dan@strathmore.edu>
2018-09-21 13:08:13 +0300

    new quizboard folder with css,bootstrap and index file

---
+++ scripts.js
@@ -0,0 +1,28 @@
+ //backend js 
+
+ 
+ 
+ 
+ 
+ //frontend js
+ $(document).ready(function(){
+     $("form#board").submit(function(event){
+         event.preventDefault();
+var output=
+     $("display").text(output);
+    });
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+ var quizboard = $("form").val();
