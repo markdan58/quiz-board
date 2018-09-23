45dafb4df59e7e3cf0f9532899954acce7aa2a41
markdan58 <mark.dan@strathmore.edu>
2018-09-23 22:51:48 +0300

    corrected all pages

--- scripts.js
+++ scripts.js
@@ -1,28 +1,23 @@
- //backend js 
+// frontend logic....>
 
+$(document).ready(function () {
+    $("form#board").submit(function (event) {
+        var q1 = $("input:radio[name=q1]:checked").val();
+        var q2 = $("input:radio[name=q2]:checked").val();
+        var q3 = $("input:radio[name=q3]:checked").val();
+        var q4 = $("input:radio[name=q4]:checked").val();
+        var q5 = $("input:radio[name=q5]:checked").val();
+        var result = parseInt(q1) + parseInt(q2) + parseInt(q3) + parseInt(q4) + parseInt(q5);
+        $("#result").text("You scored " + result);
+
+ //<..... Business logic......>
  
- 
- 
- 
- //frontend js
- $(document).ready(function(){
-     $("form#board").submit(function(event){
-         event.preventDefault();
-var output=
-     $("display").text(output);
+        $("button").click(function () {
+            $("#result-showing").toggle();
+            $("#board-hidden").toggle();
+        });
+        $("form#board").hide();
+        $("#result").show();
+        event.preventDefault();
     });
-
-
-
-
-
-
-
-
-
-
-
-
-
-
- var quizboard = $("form").val();
+});
